be8d89e2372fe6c7160afb95e8a4b60b1fdf349b
Didier Lane <didier.lane@mail.com>
2026-04-19 19:32:16 +0200

    refactor: compose.yml and docker.mk

--- make/docker.mk
+++ make/docker.mk
@@ -17,16 +17,22 @@ ALPINE_DIGEST				?= sha256:4d889c14e7d5a73929ab00be2ef8ff22437e7cbc545931e52554a
 
 DOCKER_IMAGE				?= $(COMPOSE_PROJECT_NAME):$(RUSTFS_VERSION)
 
-RUSTFS_ACCESS_KEY			?= $(shell $(random_hash))
-RUSTFS_SECRET_KEY			?= $(shell $(random_hash))
+# Docker container restart policy
+RESTART_POLICY				?= unless-stopped
+
 RUSTFS_OBS_LOGGER_LEVEL		?= info
+
+ifndef RUSTFS_ACCESS_KEY
+RUSTFS_ACCESS_KEY			!= $(random_hash)
+endif
+
+ifndef RUSTFS_SECRET_KEY
+RUSTFS_SECRET_KEY			!= $(random_hash)
+endif
 
 TLS_CA						?= ~/.ssl/local.io/CA.crt
 TLS_KEY						?= ~/.ssl/local.io/rustfs.key
 TLS_CERT					?= ~/.ssl/local.io/rustfs.crt
-
-# Docker container restart policy
-RESTART_POLICY				?= unless-stopped
 
 define get_docker_image
 docker images --format "{{.Repository}}:{{.Tag}}" | grep -E '^$(DOCKER_IMAGE)$$' 2>/dev/null

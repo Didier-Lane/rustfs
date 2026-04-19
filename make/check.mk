6a083f89ff8ee1463ac110e78ce02d6a363b1cb9
Didier Lane <didier.lane@mail.com>
2026-04-19 09:29:46 +0200

    updated check target

--- make/check.mk
+++ make/check.mk
@@ -1,5 +1,5 @@
 .PHONY: check
 check: jq # 🔄 Checks for newer versions of dependencies
 	$(call github_check_release_version,$(JQ_REPOSITORY),$(JQ_VERSION),$(JQ_ASSET))
-	$(call docker_check_version,alpine,$(ALPINE_VERSION),,edge)
-	$(call docker_check_version,rustfs/rustfs,$(RUSTFS_VERSION),,glibc)
+	$(call dockerhub_check_version,alpine,$(ALPINE_VERSION))
+	$(call dockerhub_check_version,rustfs/rustfs,$(RUSTFS_VERSION))
